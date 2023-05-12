b1a215a81068334dcf80a0701c14f7f7208c4a58
jbees918 <josh.beestoned@gmail.com>
2023-05-12 02:56:37 +0000

    c sharp

---
+++ 0-initialize_new_project.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+# Bash script that initializes a new c# project inside a folder
+
+dotnet new console -o 0-new_project
---
+++ README.md
@@ -0,0 +1,1 @@
+This is my introductory project for C sharp at Holberton!
