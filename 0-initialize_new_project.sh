9db192800f7978f59a7649aead37e04cbccfb90e
jbees918 <josh.beestoned@gmail.com>
2023-05-12 03:20:15 +0000

    fixed 0

--- 0-initialize_new_project.sh
+++ 0-initialize_new_project.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-# Bash script that initializes a new c# project inside a folder
+# Bash script that initializes new c# project inside a folder
 
 dotnet new console -o 0-new_project
