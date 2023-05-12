4322fca9ad3ff3977851d2ef456018e282725115
jbees918 <josh.beestoned@gmail.com>
2023-05-12 22:38:54 +0000

    zerofixed

--- 0-initialize_new_project.sh
+++ 0-initialize_new_project.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 # Bash script that initializes new c# project inside a folder
 
-dotnet new console -o 0-new_project
+dotnet new console -o ${1:-"0-new_project"} -f "netcoreapp2.1"
