ff7c21021552228327bd3f92d24fcbd10c6df5a9
jbees918 <109249544+jbees918@users.noreply.github.com>
2023-05-16 00:56:57 +0000

    ran .NET

--- 0-initialize_new_project.sh
+++ 0-initialize_new_project.sh
@@ -1,4 +1,20 @@
 #!/usr/bin/env bash
 # Bash script that initializes new c# project inside a folder
 
-dotnet new console -o ${1:-"0-new_project"} -f "netcoreapp2.1"
+# Define the project directory
+PROJECT_DIR="0-new_project"
+
+# Check if directory already exists
+if [ -d "$PROJECT_DIR" ]; then
+  echo "Directory $PROJECT_DIR already exists. Please remove or rename it, and then run the script again."
+  exit 1
+fi
+
+# Create the new directory
+mkdir $PROJECT_DIR
+
+# Change to the new directory
+cd $PROJECT_DIR
+
+# Initialize a new C# project
+dotnet new console
