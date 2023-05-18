1db7fc780459d385da01e3bd938f49acfbb5cb3f
jbees918 <109249544+jbees918@users.noreply.github.com>
2023-05-18 01:55:48 +0000

    task1fix

--- csharp-hello_world/1-build_new_project.sh
+++ csharp-hello_world/1-build_new_project.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-# bash script that inits and builds a project
+#All comments should go here
 
-dotnet new console -n 1-new_project
-dotnet build 1-new_project
+dotnet new console -o "1-new_project"
+dotnet build "1-new_project"
