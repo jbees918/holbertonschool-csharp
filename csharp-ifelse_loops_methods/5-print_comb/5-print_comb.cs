dad582188e6123b71848465e2ee31508125fcae0
jbees918 <109249544+jbees918@users.noreply.github.com>
2023-05-23 03:20:39 +0000

    task5

---
+++ csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
@@ -0,0 +1,12 @@
+﻿﻿using System;
+
+public class HexHex
+{
+    public static void Main(string[] args)
+    {
+        for (int i = 0; i < 100; i++)
+        {
+            Console.Write("{0}Dogs{1}", i.ToString("00"), i != 99 ? ", " : "\n");
+        }
+    }
+}
