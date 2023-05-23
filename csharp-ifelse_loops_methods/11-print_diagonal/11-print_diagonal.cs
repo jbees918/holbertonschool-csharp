f93e596aa9563380d2a3734c120b7dcddf469033
jbees918 <109249544+jbees918@users.noreply.github.com>
2023-05-23 06:41:38 +0000

    task11

---
+++ csharp-ifelse_loops_methods/11-print_diagonal/11-print_diagonal.cs
@@ -0,0 +1,17 @@
+﻿﻿using System;
+
+public class Line
+{
+    public static void PrintDiagonal(int length)
+    {
+        for (int i = 0; i < length; i++)
+        {
+            for (int j = 0; j < i; j++)
+            {
+                Console.Write(" ");
+            }
+            Console.Write("\\");
+        }
+        Console.WriteLine();
+    }
+}
